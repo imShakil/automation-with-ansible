9cb03ff1b1da749d3f6069e5d66da8085dd7eb44
Global Mobarak <mh.ice.iu@gmail.com>
2025-10-30 18:16:52 +0600

    updated: mini finance with tf modules

--- mini-finance-deployment/terraform_aws/main.tf
+++ mini-finance-deployment/terraform_aws/main.tf
@@ -1,19 +1,20 @@
 
 module "vpc" {
-  source = "git::https://github.com/imShakil/automation-with-ansible.git//epicbook-deployment/terraform/modules/vpc"
-  prefix = var.prefix
-  cidr_block = var.cidr_block
-  public_subnet = var.public_subnet_cidr
+  source      = "git::https://github.com/imShakil/tfmodules.git//aws/vpc"
+  prefix      = var.prefix
+  cidr_block  = var.cidr_block
+  subnet_size = 1
 }
 
 module "instance" {
-  source = "git::https://github.com/imShakil/automation-with-ansible.git//epicbook-deployment/terraform/modules/instance"
-  prefix = var.prefix
-  subnet_id = module.vpc.vpc_attribute.public_subnet_id
-  vpc_security_group_id = module.vpc.vpc_attribute.security_group_id
-  ami_id                = var.ami_id
-  instance_type         = var.instance_type
-  ssh_key_pair          = var.ssh_key_pair
+  source                 = "git::https://github.com/imShakil/tfmodules.git//aws/instance"
+  prefix                 = var.prefix
+  subnet_id              = module.vpc.vpc_attribute.public_subnet_ids[0]
+  vpc_security_group_ids = [module.vpc.vpc_attribute.security_group_id]
+  ami_id                 = var.ami_id
+  instance_type          = var.instance_type
+  ssh_key_pair           = var.ssh_key_pair
+  instance_number        = 1
 
 }
 
@@ -25,7 +26,7 @@ set -e
 
 cat > ../ansible/inventory.ini <<EOF
 [app]
-${module.instance.instance_attribute.public_ip}
+${module.instance.instance_attribute.public_ip[0]}
 
 [all:vars]
 ansible_user="ubuntu"
@@ -38,7 +39,7 @@ EOT
   }
 
   triggers = {
-    public_ip = module.instance.instance_attribute.public_ip
+    public_ip = module.instance.instance_attribute.public_ip[0]
   }
 
   depends_on = [module.instance]
